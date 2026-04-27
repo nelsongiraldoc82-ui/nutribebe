160d4e4fde08aaa55ad4411e62729b435ad05607
nelsongiraldoc82-ui <nelsongiraldoc82@gmail.com>
2026-04-27 13:08:09 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
